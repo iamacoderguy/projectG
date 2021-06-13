e6d87f07f80c2915dff53b9eb8e2f8a8c4f93b56
IM Coder <iamacoderguy@gmail.com>
2021-06-14 03:37:16 +0700

    Update port for heroku

--- back-end/index.ts
+++ back-end/index.ts
@@ -3,7 +3,7 @@ import app from './startup/app';
 
 console.info('To enable debug mode, set env DEBUG=funny-movies:*');
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
 module.exports = server;
