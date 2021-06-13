cc81c7eb578f8da4138913996e7a0e600635d08b
IM Coder <iamacoderguy@gmail.com>
2021-06-13 10:00:56 +0700

    Simple work with youtube data api

--- src/app/features/movie/pages/MovieListPage.tsx
+++ src/app/features/movie/pages/MovieListPage.tsx
@@ -1,42 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
+import { Movie as MovieData } from '../Movie';
 import './MovieListPage.css';
+
+// AIzaSyBqi9Po4moldIyK002uTe50vxsbxjtwOtQ
+// https://www.googleapis.com/youtube/v3/videos?id=rokGy0huYEA&key=AIzaSyBqi9Po4moldIyK002uTe50vxsbxjtwOtQ&part=snippet,contentDetails,statistics,status
+
+const sharedList = [
+  {
+    id: 'rokGy0huYEA',
+    sharedBy: 'someone@gmail.com',
+  },
+  {
+    id: '94xpmb5UvNg',
+    sharedBy: 'someone@gmail.com',
+  },
+  {
+    id: 'FPtITmtjWhQ',
+    sharedBy: 'someone@gmail.com',
+  },
+];
+
+const key = 'AIzaSyBqi9Po4moldIyK002uTe50vxsbxjtwOtQ';
 
 const MovieListPage: React.FC = (props) => {
   // TODO: handle load more
   // TODO: get info from youtube
+
+  const [ movies, setMovies ] = useState<MovieData[]>([]);
+
+  useEffect(() => {
+    const ids = sharedList.map(shared => shared.id);
+    console.log(`https://www.googleapis.com/youtube/v3/videos?id=${ids.join(',')}
+    &key=${key}
+    &part=snippet,contentDetails,statistics,status`);
+
+    fetch(`https://www.googleapis.com/youtube/v3/videos?id=${ids.join(',')}&key=${key}&part=snippet,contentDetails,statistics,status`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result.items.length);
+          setMovies(result.items.map((item: any) => {
+            return {
+              title: item.snippet.title,
+              userEmail: 'someone@gmail.com',
+              description: item.snippet.description,
+              upVotes: 86,
+              downVotes: 12,
+              youtubeId: item.id,
+            };
+          }));
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+        },
+      );
+  }, []);
+
   return (
     <div className='container movieListContainer'>
-      <Movie
-        className='movieComponent'
-        movie={{
-          title: 'Movie Title',
-          userEmail: 'someone@gmail.com',
-          upVotes: 86,
-          downVotes: 12,
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget purus vitae risus consectetur iaculis nec eget neque. Suspendisse in sem malesuada, commodo eros sit amet, dignissim nisl. Sed diam augue, suscipit at tincidunt sit amet, molestie dictum libero. Aliquam eros mi, rutrum eu gravida ut, convallis sed tortor. Praesent sed tincidunt velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas',
-          youtubeId: 'rokGy0huYEA',
-        }}/>
-      <Movie
-        className='movieComponent'
-        movie={{
-          title: 'Movie Title',
-          userEmail: 'someone@gmail.com',
-          upVotes: 86,
-          downVotes: 12,
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget purus vitae risus consectetur iaculis nec eget neque. ',
-          youtubeId: 'rokGy0huYEA',
-        }}/>
-      <Movie
-        className='movieComponent'
-        movie={{
-          title: 'Movie Title',
-          userEmail: 'someone@gmail.com',
-          upVotes: 86,
-          downVotes: 12,
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
-          youtubeId: 'rokGy0huYEA',
-        }}/>
+      {movies.map(m => {
+        return (
+          <Movie
+            key={m.youtubeId}
+            className='movieComponent'
+            movie={m}
+          />
+        );
+      },
+      )}
     </div>
   );
 };
